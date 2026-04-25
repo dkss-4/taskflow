41fd0c83d5ea2f8023a9bf8f98102fa4665c8417
Deeksha Sharma <dks.deeksha@gmail.com>
2026-04-26 01:10:39 +0530

    changes5

--- taskflow/settings.py
+++ taskflow/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
