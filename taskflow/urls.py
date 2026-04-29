efb3f2a8661a1f4bac34e8e4d9ee26f745e3db54
Deeksha Sharma <dks.deeksha@gmail.com>
2026-04-29 14:05:45 +0530

    fixing admin issue

--- taskflow/urls.py
+++ taskflow/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from tasks import views
+from .import views
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
